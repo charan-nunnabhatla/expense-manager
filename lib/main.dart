3fe305fb6257d4368220c73e8eb21bbd2bdd09b5
Charan <charan.reings125@gmail.com>
2023-04-03 20:20:37 +0530

    dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
+import 'package:expense_manager/homepage.dart';
 
 void main(List<String> args) {
   runApp(const Expense());
@@ -11,7 +10,24 @@ class Expense extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      // theme: ThemeData.from(
+      //   colorScheme: const ColorScheme(
+      //     background: Color.fromARGB(255, 0, 0, 0),
+      //     onBackground: Color.fromARGB(255, 0, 0, 0),
+      //     brightness: Brightness.dark,
+      //     primary: Color.fromARGB(255, 115, 120, 197),
+      //     onPrimary: Color.fromARGB(255, 128, 142, 206),
+      //     secondary: Color.fromARGB(255, 120, 201, 187),
+      //     onSecondary: Color.fromARGB(255, 113, 191, 194),
+      //     error: Color.fromARGB(255, 184, 122, 122),
+      //     onError: Color.fromARGB(255, 184, 122, 122),
+      //     surface: Color.fromARGB(255, 99, 190, 130),
+      //     onSurface: Color.fromARGB(255, 99, 190, 130),
+      //   ),
+      // ),
       home: ExpenseManager(),
     );
   }
@@ -22,13 +38,4 @@ class ExpenseManager extends StatefulWidget {
 
   @override
   State<ExpenseManager> createState() => ExpenseManagerState();
-}
-
-class ExpenseManagerState extends State<ExpenseManager> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Expense Manager")),
-    );
-  }
 }
