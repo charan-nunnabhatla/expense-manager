8155538dfd61bcc14f013b381e033dcf063b32f7
Charan <charan.reings125@gmail.com>
2023-04-03 19:41:26 +0530

    homepage created

---
+++ lib/homepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:expense_manager/main.dart';
+
+
+class ExpenseManagerState extends State<ExpenseManager> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Expense Manager")),
+    );
+  }
+}
