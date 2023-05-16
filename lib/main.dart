1f45cd71dc32a356688a942469188d5c12c70319
charan <charan.reings125@gmail.com>
2023-05-16 20:17:45 +0530

    renamed some classes

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,7 @@ class Expense extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true),
-      // theme: ThemeData.from(
-      //   colorScheme: const ColorScheme(
-      //     background: Color.fromARGB(255, 0, 0, 0),
-      //     onBackground: Color.fromARGB(255, 0, 0, 0),
-      //     brightness: Brightness.dark,
-      //     primary: Color.fromARGB(255, 115, 120, 197),
-      //     onPrimary: Color.fromARGB(255, 128, 142, 206),
-      //     secondary: Color.fromARGB(255, 120, 201, 187),
-      //     onSecondary: Color.fromARGB(255, 113, 191, 194),
-      //     error: Color.fromARGB(255, 184, 122, 122),
-      //     onError: Color.fromARGB(255, 184, 122, 122),
-      //     surface: Color.fromARGB(255, 99, 190, 130),
-      //     onSurface: Color.fromARGB(255, 99, 190, 130),
-      //   ),
-      // ),
-      home: ExpenseManager(),
+      home: const ExpenseManager(),
     );
   }
 }
