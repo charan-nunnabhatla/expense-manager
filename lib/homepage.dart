68cf0dec011baf2f846073644255ff68c9c3129d
charan <charan.reings125@gmail.com>
2023-05-16 20:18:25 +0530

    adding reset button -under progress

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,14 +1,16 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:expense_manager/main.dart';
 
 class ExpenseManagerState extends State<ExpenseManager> {
-  late List<bool> selected;
+  // @override
+  // void initState() {
+  // selected = [true, false];
+  // super.initState();
+  // }
+  final List<bool> isSelected = [true, false];
+  int _count = 0;
 
-  @override
   void initState() {
-    selected = [true, false];
     super.initState();
   }
 
@@ -17,26 +19,87 @@ class ExpenseManagerState extends State<ExpenseManager> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Expense Manager"),
-      ),
-      body: Column(
-        children: [
-          Center(
-            child: ToggleButtons(
-              children: [Text("Expense"), Text("Income")],
-              isSelected: selected,
-              selectedColor: Color.fromARGB(193, 82, 132, 141),
-              disabledColor: Color.fromARGB(239, 131, 130, 130),
-              fillColor: Color.fromARGB(255, 163, 160, 160),
-              onPressed: (index) {
-                setState(() {
-                  for (int i = 0; i < selected.length; i++) {
-                    selected[i] = i == index;
-                  }
-                });
-              },
-            ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                      showDragHandle: true,
+                      // isScrollControlled: true,
+                      useSafeArea: true,
+                      context: context,
+                      builder: (BuildContext context) {
+                        return StatefulBuilder(
+                            builder: (context, setModelState) {
+                          return Scaffold(
+                            persistentFooterButtons: [
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text('Next'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text('Back'),
+                              ),
+                            ],
+                            persistentFooterAlignment:
+                                AlignmentDirectional.bottomCenter,
+                            appBar: AppBar(
+                              centerTitle: true,
+                              title: Text('Bottom Sheet'),
+                              automaticallyImplyLeading: false,
+                            ),
+                            body: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Center(
+                                  child: ToggleButtons(
+                                      isSelected: isSelected,
+                                      onPressed: (index) {
+                                        setModelState(() {
+                                          for (int i = 0;
+                                              i < isSelected.length;
+                                              i++) {
+                                            isSelected[i] = i == index;
+                                          }
+                                        });
+                                      },
+                                      children: const [
+                                        Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Text('Credit'),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Text('Debit'),
+                                        )
+                                      ]),
+                                ),
+                                Text('$isSelected'),
+                                isSelected[0]
+                                    ? Text('Amount: ')
+                                    : Text('Debit: '),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      KeyedSubtree(
+                                        key: ValueKey<int>(_count),
+                                        child: ElevatedButton(
+                                          onPressed: () =>
+                                              setState(() => ++_count),
+                                          child: Text('RESET'),
+                                        ),
+                                      );
+                                    },
+                                    child: Text('Pop'))
+                              ],
+                            ),
+                          );
+                        });
+                      });
+                },
+                icon: const Icon(Icons.add_card_rounded)),
           ),
-          Text("${selected}"),
         ],
       ),
     );
