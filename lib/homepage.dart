b965616e3fcd73c418728167e53bc64abc26a874
charan <charan.reings125@gmail.com>
2023-05-17 19:15:08 +0530

    made some changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_manager/main.dart';
+import 'package:expense_manager/bottomsheet.dart';
 
 class ExpenseManagerState extends State<ExpenseManager> {
   // @override
@@ -7,12 +8,6 @@ class ExpenseManagerState extends State<ExpenseManager> {
   // selected = [true, false];
   // super.initState();
   // }
-  final List<bool> isSelected = [true, false];
-  int _count = 0;
-
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,82 +20,30 @@ class ExpenseManagerState extends State<ExpenseManager> {
             child: IconButton(
                 onPressed: () {
                   showModalBottomSheet<void>(
+                      // isScrollControlled: true,
+                      elevation: 2,
                       showDragHandle: true,
                       // isScrollControlled: true,
                       useSafeArea: true,
                       context: context,
                       builder: (BuildContext context) {
-                        return StatefulBuilder(
-                            builder: (context, setModelState) {
-                          return Scaffold(
-                            persistentFooterButtons: [
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text('Next'),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text('Back'),
-                              ),
-                            ],
-                            persistentFooterAlignment:
-                                AlignmentDirectional.bottomCenter,
-                            appBar: AppBar(
-                              centerTitle: true,
-                              title: Text('Bottom Sheet'),
-                              automaticallyImplyLeading: false,
-                            ),
-                            body: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Center(
-                                  child: ToggleButtons(
-                                      isSelected: isSelected,
-                                      onPressed: (index) {
-                                        setModelState(() {
-                                          for (int i = 0;
-                                              i < isSelected.length;
-                                              i++) {
-                                            isSelected[i] = i == index;
-                                          }
-                                        });
-                                      },
-                                      children: const [
-                                        Padding(
-                                          padding: EdgeInsets.all(10),
-                                          child: Text('Credit'),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.all(10),
-                                          child: Text('Debit'),
-                                        )
-                                      ]),
-                                ),
-                                Text('$isSelected'),
-                                isSelected[0]
-                                    ? Text('Amount: ')
-                                    : Text('Debit: '),
-                                ElevatedButton(
-                                    onPressed: () {
-                                      KeyedSubtree(
-                                        key: ValueKey<int>(_count),
-                                        child: ElevatedButton(
-                                          onPressed: () =>
-                                              setState(() => ++_count),
-                                          child: Text('RESET'),
-                                        ),
-                                      );
-                                    },
-                                    child: Text('Pop'))
-                              ],
-                            ),
-                          );
-                        });
+                        return BottomSheetState();
                       });
                 },
                 icon: const Icon(Icons.add_card_rounded)),
           ),
         ],
+      ),
+      body: ListView.builder(
+        itemCount: 200,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('${index+1}'),
+            ),
+          );
+        },
       ),
     );
   }
