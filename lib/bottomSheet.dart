4f5e9e26e237dbb694c2474a59bab6bde09ceef4
charan <charan.reings125@gmail.com>
2023-05-17 12:37:40 +0530

    separated the code into compoents

---
+++ lib/bottomSheet.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class BottomSheet extends StatefulWidget {
+  const BottomSheet({super.key});
+
+  @override
+  State<BottomSheet> createState() => BottomSheetState();
+}
+
+class BottomSheetState extends State<BottomSheet> {
+  final List<bool> isSelected = [true, false];
+
+  @override
+  Widget build(BuildContext context) {
+    return StatefulBuilder(builder: (context, setModelState) {
+      return Scaffold(
+        persistentFooterButtons: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Next'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Back'),
+          ),
+        ],
+        persistentFooterAlignment: AlignmentDirectional.bottomCenter,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Bottom Sheet'),
+          automaticallyImplyLeading: false,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Center(
+              child: ToggleButtons(
+                  isSelected: isSelected,
+                  onPressed: (index) {
+                    setModelState(() {
+                      for (int i = 0; i < isSelected.length; i++) {
+                        isSelected[i] = i == index;
+                      }
+                    });
+                  },
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text('Credit'),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text('Debit'),
+                    )
+                  ]),
+            ),
+            Text('$isSelected'),
+            isSelected[0] ? Text('Amount: ') : Text('Debit: '),
+            ElevatedButton(onPressed: () {}, child: Text('Pop'))
+          ],
+        ),
+      );
+    });
+  }
+}
